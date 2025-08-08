9e90a76d62fbe9384489854b2bebe4a68bf2dd12
Witcher0766 <kumarvishwajeet076@gmail.com>
2025-08-08 16:49:48 +0530

    fixes

--- app/routes.ts
+++ app/routes.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import userRoutes from "./user/user.route";
 import documentRoutes from "./document/document.route";
+import subscriptionRoutes from "./subscription/subscription.route";
 
 const router = express.Router();
 
 router.use("/users", userRoutes);
 router.use("/documents", documentRoutes);
 // router.use("/subscription/webhook", express.raw({ type: "application/json" }));
-
+router.use('/subscription', subscriptionRoutes);
 
 export default router;
